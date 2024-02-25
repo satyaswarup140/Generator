42eb23878be0e50817c1a6d9a11da0d355aeba3c
satyaswarup140 <dsatyaswarup@gmail.com>
2024-02-25 15:58:55 +0000

    solution

---
+++ src/components/GradientGenerator/styledComponents.js
@@ -0,0 +1,53 @@
+// Style your elements here
+import styled from 'styled-components/macro'
+
+export const GradientGeneratorContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-image: linear-gradient(${props => props.gradientValue});
+  min-height: 100vh;
+`
+
+export const ResponsiveContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 90%;
+  max-width: 550px;
+  @media screen and (min-width: 768px) {
+    width: 85%;
+    max-width: 1110px;
+  }
+`
+
+export const Heading = styled.h1`
+  color: #ffffff;
+  font-family: 'Roboto';
+  font-weight: bold;
+  text-align: center;
+  font-size: 24px;
+  @media screen and (min-width: 768px) {
+    font-size: 32px;
+  }
+`
+
+export const DirectionsDescription = styled.p`
+  color: #ededed;
+  font-family: 'Roboto';
+  font-weight: 500;
+  font-size: 22px;
+  line-height: 1.5;
+`
+
+export const GradientDirectionList = styled.ul`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  padding: 0;
+  @media screen and (min-width: 768px) {
+    width: 60%;
+    max-width: 425px;
+  }
+`
